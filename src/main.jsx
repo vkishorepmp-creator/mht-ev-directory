8bd7393b5c1a7fd1fdc5c902af7d3ae629d272d3
vkishorepmp-creator <vkishorepmp@gmail.com>
2026-05-25 01:45:57 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import MHTEVDirectory from './MHT_EV_Directory'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode><MHTEVDirectory /></StrictMode>
+)
